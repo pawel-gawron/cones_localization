7bb9a804324663f0dd5f12a7f541c69b2ddd4149
Paweł Gawron <gawron.pawel1999@gmail.com>
2024-04-06 23:39:37 +0200

    merging lidar and camera data

--- include/cones_localization/cones_localization_node.hpp
+++ include/cones_localization/cones_localization_node.hpp
@@ -17,8 +17,19 @@
 
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
+
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 
 #include "cones_localization/cones_localization.hpp"
+#include "cones_interfaces/msg/bounding_box.hpp" 
+#include "cones_interfaces/msg/cones.hpp" 
 
 namespace cones_localization
 {
@@ -32,6 +43,33 @@ public:
 private:
   ConesLocalizationPtr cones_localization_{nullptr};
   int64_t param_name_{123};
+  int image_width_;
+  int image_height_;
+  bool executed_ = false;
+
+  float max_x_camera_;
+  float max_y_camera_;
+  float min_x_camera_;
+  float min_y_camera_;
+  float camera_fov_horizontal_;
+  float camera_fov_vertical_;
+
+  float angle_max_;
+  float angle_min_;
+
+  std::vector<std::pair<float, float>> lidar_points_;
+  float max_range;
+
+  void bboxesCallback(const cones_interfaces::msg::Cones::SharedPtr msg);
+  void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg);
+  void lidarCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+  void localizationCallback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg);
+  void cameraInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr msg);
+  rclcpp::Subscription<cones_interfaces::msg::Cones>::SharedPtr bboxes_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr localization_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr camera_sub_;
 };
 }  // namespace cones_localization
 
--- src/cones_localization_node.cpp
+++ src/cones_localization_node.cpp
@@ -16,6 +16,7 @@
 
 namespace cones_localization
 {
+auto custom_qos = rclcpp::QoS(rclcpp::KeepLast(1), rmw_qos_profile_sensor_data);
 
 ConesLocalizationNode::ConesLocalizationNode(const rclcpp::NodeOptions & options)
 :  Node("cones_localization", options)
@@ -23,6 +24,180 @@ ConesLocalizationNode::ConesLocalizationNode(const rclcpp::NodeOptions & options
   cones_localization_ = std::make_unique<cones_localization::ConesLocalization>();
   param_name_ = this->declare_parameter("param_name", 456);
   cones_localization_->foo(param_name_);
+
+  bboxes_sub_ = this->create_subscription<cones_interfaces::msg::Cones>(
+  "/output_bboxes",
+  custom_qos,
+  std::bind(&ConesLocalizationNode::bboxesCallback, this, 
+  std::placeholders::_1));
+
+  image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+  "/output_image",
+  custom_qos,
+  std::bind(&ConesLocalizationNode::imageCallback, this, 
+  std::placeholders::_1));
+
+  lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+  "/sensing/lidar/scan",
+  custom_qos,
+  std::bind(&ConesLocalizationNode::lidarCallback, this, 
+  std::placeholders::_1));
+
+  localization_sub_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
+  "/localization/pose_twist_fusion_filter/pose_with_covariance",
+  custom_qos,
+  std::bind(&ConesLocalizationNode::localizationCallback, this, 
+  std::placeholders::_1));
+
+  camera_sub_ = this->create_subscription<sensor_msgs::msg::CameraInfo>(
+  "/sensing/camera/camera_info",
+  custom_qos,
+  std::bind(&ConesLocalizationNode::cameraInfoCallback, this, 
+  std::placeholders::_1));
+}
+
+void ConesLocalizationNode::cameraInfoCallback(const sensor_msgs::msg::CameraInfo::SharedPtr msg)
+{
+    if (!executed_)
+    {
+      float fx = msg->k[0];
+      float cx = msg->k[2];
+      float fy = msg->k[4];
+      float cy = msg->k[5]; 
+      image_width_ = msg->width;
+      image_height_ = msg->height;
+
+      max_x_camera_ = (image_width_ - 1 - cx) / fx;
+      min_x_camera_ = -cx / fx;
+      angle_max_ = std::atan(max_x_camera_ / fx);
+
+      camera_fov_horizontal_ = 2 * std::atan(image_width_ / (2 * fx));
+      camera_fov_vertical_ = 2 * std::atan(image_height_ / (2 * fy));
+
+      max_y_camera_ = (image_height_ - 1 - cy) / fy;
+      min_y_camera_ = -cy / fy;
+
+      angle_min_ = -angle_max_;
+
+      executed_ = true;
+    }
+}
+
+void ConesLocalizationNode::bboxesCallback(const cones_interfaces::msg::Cones::SharedPtr msg)
+{
+  // RCLCPP_INFO(this->get_logger(), "Received bboxes message");
+  // Print the message to the output
+  // You can access the message data using msg->data or other relevant fields
+  // Example: std::cout << "Message data: " << msg->data << std::endl;
+  cones_interfaces::msg::Cones cones;
+  cones.header = msg->header;
+  // RCLCPP_INFO(this->get_logger(), cones.header);
+  std::cout << "Message data: " << msg->header.frame_id << std::endl;
+}
+
+void ConesLocalizationNode::imageCallback(const sensor_msgs::msg::Image::SharedPtr msg)
+{
+  // RCLCPP_INFO(this->get_logger(), "Received image message");
+  cv::Mat frame_cv;
+  frame_cv = cv_bridge::toCvCopy(msg, "bgr8")->image;
+
+  for (const auto& point : lidar_points_)
+  {
+    // std::cout << "Point x: " << point.first << " y: " << point.second << std::endl;
+    cv::circle(frame_cv, cv::Point(point.first, point.second), 2, cv::Scalar(0, 255, 0), -1);
+  }
+
+  cv::imshow("Cones from localization", frame_cv);
+  cv::waitKey(1);
+}
+
+void ConesLocalizationNode::lidarCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+{
+  RCLCPP_INFO(this->get_logger(), "Received lidar message");
+
+  auto scan_msg = std::make_unique<sensor_msgs::msg::LaserScan>();
+  scan_msg->header = msg->header;
+
+  scan_msg->angle_min = msg->angle_min;
+  scan_msg->angle_max = msg->angle_max;
+  scan_msg->angle_increment = msg->angle_increment;
+  scan_msg->time_increment = msg->time_increment;
+  scan_msg->scan_time = msg->scan_time;
+  scan_msg->range_min = msg->range_min;
+  scan_msg->range_max = msg->range_max;
+
+  max_range = msg->range_max;
+
+  // const int no_data = -1;
+  // std::vector<int> v_pointcloud_index;
+
+  // determine amount of rays to create
+  uint32_t ranges_size = std::ceil(
+    (scan_msg->angle_max - scan_msg->angle_min) / scan_msg->angle_increment);
+
+  std::cout << "ranges_size: " << ranges_size << std::endl;
+
+
+  lidar_points_.clear();
+
+  float min_angle = msg->angle_min;
+  float max_angle = msg->angle_max;
+  float angle_increment = msg->angle_increment;
+
+  // float lidar_fov = max_angle - min_angle; 
+
+  std::vector<float> ranges = msg->ranges;
+
+  // const float lidar_height = 100;  // Lidar height above ground (meters)
+  // const float camera_offset = 0.1; // Camera offset below lidar (meters)
+
+  for (uint32_t i = 0; i < ranges.size(); ++i)
+  { 
+    float angle = min_angle + i * angle_increment;    
+    float distance = ranges[i]; 
+
+    // std::cout << "angle: " << angle << std::endl;
+    // std::cout << "min_angle" << min_angle << std::endl;
+    // std::cout << "max_angle" << max_angle << std::endl;
+    // std::cout << "camera_fov_" << camera_fov_ << std::endl;
+    // std::cout << "max_angle - min_angle" << max_angle - min_angle << std::endl;
+
+    // Check if point is within camera FOV 
+    if (angle >= min_angle && angle <= max_angle && angle <= camera_fov_horizontal_/2 && angle >= -camera_fov_horizontal_/2)
+    {
+        // Normalize angle to be between 0 and 1 relative to camera FOV
+        float normalized_angle = ((angle+(camera_fov_horizontal_/2)) / (camera_fov_horizontal_));
+
+        // Estimate image x-coordinate 
+        int x_pixel = static_cast<int>(normalized_angle * image_width_); 
+
+        // Assuming fixed y position for now 
+        // int y_pixel = image_height_ / 2;
+
+        float normalized_distance = distance / 30;
+
+        // Invert and scale for y_pixel calculation
+        float normalized_y = 1.0 - normalized_distance;  // 1.0 is top, 0.0 is bottom
+        int y_pixel = static_cast<int>((normalized_y) * image_height_);
+
+
+        std::cout << "x_pixel: " << x_pixel << std::endl;
+        std::cout << "y_pixel: " << y_pixel << std::endl;
+
+        lidar_points_.emplace_back(x_pixel, y_pixel); 
+    }  
+  }
+}
+
+void ConesLocalizationNode::localizationCallback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
+{
+  RCLCPP_INFO(this->get_logger(), "Received localization message");
+  geometry_msgs::msg::PoseWithCovarianceStamped imu;
+  imu.header = msg->header;
+  // RCLCPP_INFO(this->get_logger(), imu.header);
+  std::cout << "Message data: " << msg->header.frame_id << std::endl;
+
+
 }
 
 }  // namespace cones_localization
